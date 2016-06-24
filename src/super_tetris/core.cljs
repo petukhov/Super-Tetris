acf329cf33549d1f155aa60a58099326925f7bfb
Georgy Petukhov <gpetukhov@me.com>
2016-06-24 01:35:32 -0500

    rectangle is moving!

--- src/super_tetris/core.cljs
+++ src/super_tetris/core.cljs
@@ -16,3 +16,17 @@
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
 
+(def canvas (.getElementById js/document "canvas"))
+(def context (.getContext canvas "2d"))
+(set! (.-fillStyle context) "blue")
+(.fillRect context 10 10 100 100)
+
+
+(defn animation-loop [state]
+  ;(println "animation is happening" (state :x) (state :y))
+  (.clearRect context 0 0 500 500)
+  (.fillRect context (state :x) (state :y) 100 100)
+  (let [new-state (update state :x + 1)]
+    (.requestAnimationFrame js/window (partial animation-loop new-state))))
+
+(.requestAnimationFrame js/window (partial animation-loop {:x 20 :y 10}))
