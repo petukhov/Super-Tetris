a40f98496446ad8fdad275d9eaaad6a6fa9c143c
Georgy Petukhov <gpetukhov@me.com>
2016-06-27 01:47:48 -0500

    gen-game-map is half complete

--- src/super_tetris/core.cljs
+++ src/super_tetris/core.cljs
@@ -17,23 +17,35 @@
 (events/listen js/document "keydown" #(put! events-chan :keydown))
 (.setInterval js/window #(put! events-chan :tick) 300)
 
+(def initial-state  {:x 20 :y 10 :dx 30})
+
+(defn gen-game-map [vertical-count horizontal-count sq-height sq-width]
+  (let [horizontal-range (range horizontal-count)
+        vertical-range (range vertical-count)
+        empty-2d-vec (vec (map #(vec (repeat horizontal-count {})) vertical-range))
+        indices (for [i horizontal-range
+                      j vertical-range]
+                  [i j])
+        add-fields (fn [acc curr] (assoc-in acc (conj curr :state) :show))]
+    (reduce add-fields empty-2d-vec indices)))
+
 (defn render [state]
   (.clearRect context 0 0 500 500)
   (.fillRect context (get state :x) (get state :y) 100 100)
   (println "render"))
 
-(defn process-events [state last-event]
+(defn update-state-after-event [state last-event]
   (case (get last-event 0)
     :keydown [true (update state :dx * -1)]
     :tick    [true (update state :x + (get state :dx))]
     :nothing [false state]
     :default))
 
-(defn animation-loop [state]
-  (go
-    (let [last-event (alts! [events-chan] :default :nothing)
-          [should-update? new-state] (process-events state last-event)]
-      (when should-update? (render new-state))
-      (.requestAnimationFrame js/window (partial animation-loop new-state)))))
+((fn animation-loop [state]
+   (go
+     (let [last-event (alts! [events-chan] :default :nothing)
+           [should-update? new-state] (update-state-after-event state last-event)]
+       (when should-update? (render new-state))
+       (.requestAnimationFrame js/window (partial animation-loop new-state)))))
+  initial-state)
 
-(.requestAnimationFrame js/window (partial animation-loop {:x 20 :y 10 :dx 30}))
