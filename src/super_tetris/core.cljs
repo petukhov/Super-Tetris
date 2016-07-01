f35ca4e0f69b2100a5e47b9c2b85c3c89dc7ad03
Georgy Petukhov <gpetukhov@me.com>
2016-07-01 02:26:30 -0500

    rendering the board of squares

--- src/super_tetris/core.cljs
+++ src/super_tetris/core.cljs
@@ -6,21 +6,7 @@
 
 (enable-console-print!)
 (declare .requestAnimationFrame)
-
-;;constants
-(def gap-width 2)
-(def initial-state  {:x 20 :y 10 :dx 30})
-
-;;setting up the canvas
-(def canvas (.getElementById js/document "canvas"))
-(def context (.getContext canvas "2d"))
-(set! (.-fillStyle context) "blue")
-(.fillRect context 10 10 100 100)
-
-;;event listeners
-(def events-chan (chan 5))
-(events/listen js/document "keydown" #(put! events-chan :keydown))
-(.setInterval js/window #(put! events-chan :tick) 300)
+(declare gap-width)
 
 (defn create-square [h-pos v-pos sq-size]
   {:x (+ (* h-pos sq-size) (* h-pos gap-width))
@@ -28,21 +14,41 @@
    :shown true})
 
 (defn gen-game-map [vertical-count horizontal-count sq-size]
-  (->> (for [i (range horizontal-count), k (range vertical-count)]
-          (create-square i k sq-size))
+  (->> (for [i (range horizontal-count)
+             k (range vertical-count)]
+         (create-square i k sq-size))
        (partition vertical-count)
        (map #(vec %))
        (vec)))
 
+;;constants
+(def gap-width 2)
+(def initial-state (gen-game-map 10 10 5))
+
+;;setting up the canvas
+(def canvas (.getElementById js/document "canvas"))
+(def context (.getContext canvas "2d"))
+(set! (.-fillStyle context) "blue")
+
+;;event listeners
+(def events-chan (chan 5))
+(events/listen js/document "keydown" #(put! events-chan :keydown))
+(.setInterval js/window #(put! events-chan :tick) 300)
+
+(defn draw-square [square]
+  (when (get square :shown)
+    (.fillRect context (get square :x) (get square :y) 5 5)))
+
 (defn render [state]
   (.clearRect context 0 0 500 500)
-  (.fillRect context (get state :x) (get state :y) 100 100)
-  (println "render"))
+  (doall (for [sub-vector state
+         square sub-vector]
+     (draw-square square))))
 
 (defn update-state-after-event [state last-event]
   (case (get last-event 0)
-    :keydown [true (update state :dx * -1)]
-    :tick    [true (update state :x + (get state :dx))]
+    :keydown [true (update-in state [6 6 :shown] not)]
+    :tick    [true state]
     :nothing [false state]
     :default))
 
