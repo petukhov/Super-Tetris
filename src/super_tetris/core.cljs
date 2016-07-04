e5c86270538852d0f1e9fb8ce7c828fcb00341eb
Georgy Petukhov <gpetukhov@me.com>
2016-07-04 03:14:29 -0500

    made the canvas size adjust according to the square size. put some magic numbers into the constants

--- src/super_tetris/core.cljs
+++ src/super_tetris/core.cljs
@@ -6,7 +6,12 @@
 
 (enable-console-print!)
 (declare .requestAnimationFrame)
-(declare gap-width)
+(declare init-game-state)
+
+;;functions related to setting up the game state
+(defn calculate-dimensions [vertical-count horizontal-count sq-size]
+  [(+ (* vertical-count sq-size) (* vertical-count gap-width) (- gap-width))
+   (+ (* horizontal-count sq-size) (* horizontal-count gap-width) (- gap-width))])
 
 (defn create-square [h-pos v-pos sq-size]
   {:x (+ (* h-pos sq-size) (* h-pos gap-width))
@@ -21,35 +26,49 @@
        (map #(vec %))
        (vec)))
 
+(defn init-game-state [& args]
+  (let [[height width] (apply calculate-dimensions args)]
+    {:game-map (apply gen-game-map args)
+     :width width
+     :height height}))
+
 ;;constants
 (def gap-width 2)
-(def initial-state (gen-game-map 10 10 5))
+(def horizontal-count 10)
+(def vertical-count 10)
+(def sq-size 15)
+(def tick-interval 300)
+(def initial-state (init-game-state vertical-count horizontal-count sq-size))
 
 ;;setting up the canvas
 (def canvas (.getElementById js/document "canvas"))
 (def context (.getContext canvas "2d"))
 (set! (.-fillStyle context) "blue")
+(let [{:keys [width height]} initial-state]
+  (set! (.-width canvas) width)
+  (set! (.-height canvas) height))
 
 ;;event listeners
 (def events-chan (chan 5))
 (events/listen js/document "keydown" #(put! events-chan :keydown))
-(.setInterval js/window #(put! events-chan :tick) 300)
+(.setInterval js/window #(put! events-chan :tick) tick-interval)
 
+;;functions related to rendering
 (defn draw-square [square]
   (when (get square :shown)
-    (.fillRect context (get square :x) (get square :y) 5 5)))
+    (.fillRect context (get square :x) (get square :y) sq-size sq-size)))
 
-(defn render [state]
-  (.clearRect context 0 0 500 500)
+(defn render [{:keys [game-map width height]}]
+  (.clearRect context 0 0 width height)
   (doall
-    (for [sub-vector state
+    (for [sub-vector game-map
           square sub-vector]
       (draw-square square)))
   nil)
 
 (defn update-state-after-event [state last-event]
   (case (get last-event 0)
-    :keydown [true (update-in state [6 6 :shown] not)]
+    :keydown [true (update-in state [:game-map 6 6 :shown] not)]
     :tick    [true state]
     :nothing [false state]
     :default))
@@ -61,4 +80,3 @@
        (when should-update? (render new-state))
        (.requestAnimationFrame js/window (partial animation-loop new-state)))))
   initial-state)
-
